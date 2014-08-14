557032042d062e990ea329f143ad833ab60fe30c
austenc <austen.cameron@gmail.com>
2014-08-14 00:31:03 -0600

    took out project-specific DB stuff

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-echo "---------------- Setting Up Databases ----------------\n"
+echo "-------- Reconfiguring Database --------\n"
 
 # Reset the mysql root user password to blank
 mysql -uroot -p"secret" -Bse "use mysql; UPDATE user SET password=PASSWORD('') WHERE User='root'; FLUSH PRIVILEGES;"
-
-# Setup databases for testmaster
-mysql -u root -Bse "CREATE DATABASE IF NOT EXISTS testmaster;"
-mysql -u root -Bse "CREATE DATABASE IF NOT EXISTS testbank;"
-mysql -u root -Bse "CREATE DATABASE IF NOT EXISTS codeception;" # Used for Testing
 
 # install phpmyadmin
 echo phpmyadmin       phpmyadmin/reconfigure-webserver  text              | sudo debconf-set-selections
@@ -17,3 +12,9 @@ echo phpmyadmin       phpmyadmin/mysql/admin-pass       password          | sudo
 echo phpmyadmin       phpmyadmin/mysql/app-pass         password          | sudo debconf-set-selections
 sudo apt-get install -y phpmyadmin
 sed -ire '/\$cfg\['\''Servers'\''\]\[\$i\]\['\''AllowNoPassword'\''\] = TRUE/ s/^.*\/\/ /    /' /etc/phpmyadmin/config.inc.php
+
+# upgrade npm
+npm update -g npm
+
+# install bower
+npm install -g bower
